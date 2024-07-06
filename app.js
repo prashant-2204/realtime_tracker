f806d7a962c03b71a14db15f042bfeb08beb4490
prashant-2204 <pk6710@srmist.edu.in>
2024-07-06 15:16:08 +0530

    first

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const socketIo = require('socket.io');
+
+const server=http.createServer(app);
+const io = socketIo(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  socket.on('send-location', (data) => {
+    io.emit('receive-location', {id:socket.id,...data})});
+
+    socket.on('disconnect', () => {
+      io.emit('user-disconnected', socket.id);
+    });
+  console.log('New user connected');
+})
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+})
